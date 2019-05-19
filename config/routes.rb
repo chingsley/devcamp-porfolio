a62301e98dc881d7092ba782ec2704eff01c9872
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro-2.local>
2019-05-19 14:41:55 +0100

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  #rails-generated routes
   resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # customized routes
+  get 'about-me', to: 'pages#about'    #i.e map '[host]/about' to pages controller -> about action
+  get 'contact', to: 'pages#contact'    #i.e map '[host]/contact' to pages controller -> contact action
+  root to: 'pages#home'     # maps the homepage [host] to pages controller -> home action, and therefore to pages/home view
 end
