9322bcf5261b8dc6f0a5e2608e642f0fc4a26c3e
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro-2.local>
2019-05-19 17:00:01 +0100

    integrate friendly route to blog post

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "10 blog posts created"
 my_skills = ["Javascript", "Ruby", "React and Redux", "nodeJS", "HTML"]
 5.times do |i|
   Skill.create!(
-    title: "#{my_skills[i]}",
+    title: "#{my_skills[i+1]}",
     percent_utilized: 4 * (i + 1)
   )
 end
@@ -26,8 +26,8 @@ puts "5 skills createe"
 
 9.times do |i|
   Portfolio.create!(
-    title: "Portfolio title #{i}",
-    subtitle: "My greate services no #{i}",
+    title: "Portfolio title #{i+1}",
+    subtitle: "My greate services no #{i+1}",
     body: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
