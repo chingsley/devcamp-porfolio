50b2aa3fe8d1fbb7c1058942f14a42323499c6b6
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro-2.local>
2019-06-18 17:34:03 +0100

    implement href parser for tweets

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+  def twitter_parser tweet
+    regex = %r{\b(?:https?:\/\/)?(?i)[a-z][a-z0-9_-]*(?:[.\/](?!http)[a-z0-9_-]+)+\/?(?:\?[^\s,.]+)?}ix
+
+    # gsub is a method in rails called 'global substitution'
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+
+  end
 end
