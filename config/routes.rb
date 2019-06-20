e9b5f45e84f94bbf7dcd572cb5350f73feade16f
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro-2.local>
2019-06-20 16:03:29 +0100

    implement action cable for real-time comment posting

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # overrides the default route to 'show', after setting the 'except' in the line directly above
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  # customized routes
+  get 'about-me', to: 'pages#about'    #i.e map '[host]/about' to pages controller -> about action
+  get 'contact', to: 'pages#contact'    #i.e map '[host]/contact' to pages controller -> contact action
+  get 'tech-news', to: 'pages#tech_news'    #i.e map '[host]/contact' to pages controller -> contact action
 
   resources :blogs do
     member do
@@ -15,11 +17,6 @@ Rails.application.routes.draw do
     end
   end
   
-  # customized routes
-  get 'about-me', to: 'pages#about'    #i.e map '[host]/about' to pages controller -> about action
-  get 'contact', to: 'pages#contact'    #i.e map '[host]/contact' to pages controller -> contact action
-  get 'tech-news', to: 'pages#tech_news'    #i.e map '[host]/contact' to pages controller -> contact action
-
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'     # maps the homepage [host] to pages controller -> home action, and therefore to pages/home view
