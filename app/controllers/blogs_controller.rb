a3726b5941c943ad0308a1244cd0ceedd1dc098a
Kingsley Eneja <kingsleyeneja@Kingsleys-MacBook-Pro-2.local>
2019-06-21 15:40:07 +0100

    impelement blog topic widget: a list of the topics that has blogs associated with them. Those topics are listed in the side bar

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :destroy, :toggle_status]
   layout "blog"  # This is the same as layout("blog"). layout is a method that takes "blog" as an argument
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
@@ -91,5 +92,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
+    end
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
